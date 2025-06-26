03293f7cebeb47eb6c32310912ac1561fa4b5be6
Tanvir 321110 <tanvirahmed321110@gmail.com>
2025-06-26 17:41:52 +0600

    dynamic added category

--- controllers/blog.py
+++ controllers/blog.py
@@ -10,13 +10,21 @@ class BlogModule(http.Controller):
         categories = req.env['tourgull_blog.blog.category'].search([])
 
         tag_id = kw.get('tag_id')
+        category_id = kw.get('category_id')
+
+        blog_list = []
+
         if tag_id and tag_id.isdigit():
-            blogs = req.env['tourgull_blog.blog'].search([('tag_ids', 'in', int(tag_id))])
-        else:
-            blogs = req.env['tourgull_blog.blog'].search([])
+            blog_list.append(('tag_ids', 'in', int(tag_id)))
+
+        if category_id and category_id.isdigit():
+            blog_list.append(('category_ids', 'in', int(category_id)))
+
+        blogs = req.env['tourgull_blog.blog'].search(blog_list)
 
         return req.render('tourgull_blog.blog_page', {
             'blog_data': blog_data,
             'categories': categories,
             'blogs': blogs,
         })
+
